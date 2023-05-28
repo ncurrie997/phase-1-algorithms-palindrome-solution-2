83f5aef0297d19267b5f99e829699b7e4151bd28
Nick Currie <gosox997@gmail.com>
2023-05-28 14:58:49 -0400

    This is so fungit add . all tests passed

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length -1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
+
+  step 1: get length of word
+  step 2: get half length of word to determine middle letter index
+  step 3: iterate over the word to see if the first and last letters match
+  step 4: repeat step 3 until you reach the middle letter
+  step 5: return false if any letters dont match stopping the function 
+  step 6: return true if all letters match
 */
 
 /*
   Add written explanation of your solution here
+
+  my code takes a word and finds the length in turn finding the middle letter
+  then we test the index of the first and last, second to secondlast, and so on to see if the letters match
+  returns true if all letters match making the word a Palindrome and false if they dont making it not a palindrome
+
 */
 
 // You can run `node index.js` to view these console logs
